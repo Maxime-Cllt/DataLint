faeb403eb990654c345c7af3ee1f81596b80ebf9
Maxime Colliat <mcolliat21@gmail.com>
2025-06-17 16:49:01 +0200

    refacto code

--- src/utils/util.rs
+++ src/utils/util.rs
@@ -7,7 +7,7 @@ use std::io::{Error, ErrorKind};
 use std::path::PathBuf;
 use std::time::Instant;
 
-/// Génère un fichier JSON contenant les anomalies détectées
+/// Create a JSON file with the analysis results.
 pub fn generate_json_file(
     dangerous_output: Vec<Anomaly>,
     regex_analyze: u32,
@@ -21,7 +21,7 @@ pub fn generate_json_file(
         if let Err(e) = std::fs::create_dir(JSON_DIR) {
             log_and_print_message(
                 format!(
-                    "Une erreur est survenue lors de la création du dossier {}: {e}",
+                    "Error while creating the JSON directory: {e}. Please create the directory manually at: {}",
                     JSON_DIR
                 )
                 .as_str(),
@@ -44,7 +44,7 @@ pub fn generate_json_file(
 
     if let Err(e) = json_response.save_to_file(&save_path) {
         log_and_print_message(
-            format!("Une erreur est survenue lors de la sauvegarde du fichier JSON: {e}").as_str(),
+            format!("Error while saving the JSON file: {e}").as_str(),
             LogLevel::Error,
         );
         return;
@@ -53,7 +53,7 @@ pub fn generate_json_file(
     if cfg!(debug_assertions) {
         print_message(
             format!(
-                "Fichier JSON généré dans le dossier: {}{}/{JSON_DIR}{}",
+                "JSON file created at : {}{}/{JSON_DIR}{}",
                 Color::Red,
                 &current_dir,
                 Color::Reset
@@ -64,33 +64,33 @@ pub fn generate_json_file(
     }
 }
 
-/// Récupère le chemin du fichier CSV à analyser depuis les arguments passés au programme
+/// Get the CSV file path and output JSON file name from command line arguments.
 pub fn get_file_from_args(args: &[String]) -> Result<[String; 2], Error> {
     if args.len() != 3 {
         return Err(Error::new(
             ErrorKind::InvalidInput,
-            "Il faut passer en argument le chemin du fichier CSV à analyser et le nom du fichier PDF en sortie",
+            "Error you must provide exactly 2 arguments: the CSV file path and the output JSON file name.",
         ));
     }
 
     if !file_exists(&args[1]) {
         return Err(Error::new(
             ErrorKind::NotFound,
-            format!("Le fichier {} n'existe pas", &args[1]),
+            format!("File {} does not exist", &args[1]),
         ));
     }
 
     if !args[1].ends_with(".csv") {
         return Err(Error::new(
             ErrorKind::InvalidInput,
-            format!("Le fichier {} doit être au format CSV", &args[1]),
+            format!("File {} must be in CSV format", &args[1]),
         ));
     }
 
     if !args[2].ends_with(".json") {
         return Err(Error::new(
             ErrorKind::InvalidInput,
-            format!("Le fichier {} doit être au format JSON", &args[2]),
+            format!("File {} must be in JSON format", &args[2]),
         ));
     }
 
@@ -99,19 +99,19 @@ pub fn get_file_from_args(args: &[String]) -> Result<[String; 2], Error> {
     Ok([csv_file_path, output_name])
 }
 
-/// Affiche le rapport d'analyse dans la console et le fichier de log
+/// Display a report of the analysis results, including the number of anomalies found, the CSV file analyzed, and the execution time.
 pub fn print_report(
     start_time: &Instant,
     dangerous_output: &[Anomaly],
     csv_file_path: &str,
     debug: bool,
 ) {
-    let nombre_anomalies: usize = dangerous_output.len();
+    let anomalies_count: usize = dangerous_output.len();
     if debug {
         Anomaly::print_result(dangerous_output);
         print_message(
             format!(
-                "Nombre d'anomalies: {}{nombre_anomalies}{}",
+                "Number of anomalies found: {}{anomalies_count}{}",
                 Color::Red,
                 Color::Reset
             )
@@ -120,7 +120,7 @@ pub fn print_report(
         );
         print_message(
             format!(
-                "Fichier CSV analysé: {}{csv_file_path}{}",
+                "Analyzed file: {}{csv_file_path}{}",
                 Color::Red,
                 Color::Reset
             )
@@ -129,7 +129,7 @@ pub fn print_report(
         );
         print_message(
             format!(
-                "Temps d'exécution: {}{:?}{}",
+                "Execution time: {}{:?}{}",
                 Color::Blue,
                 start_time.elapsed(),
                 Color::Reset
@@ -139,26 +139,23 @@ pub fn print_report(
         );
     }
     log_message(
-        format!("Nombre d'anomalies: [{nombre_anomalies}] dans le fichier : {}, Temps d'exécution: [{:?}]", get_file_name(csv_file_path), start_time.elapsed()).as_str(), &LogLevel::Info,
+        format!("Anomalies: [{anomalies_count}] in file : {}, Execution time: [{:?}]", get_file_name(csv_file_path), start_time.elapsed()).as_str(), &LogLevel::Info,
     );
 }
 
-/// Exécute les actions post-analyse (suppression des fichiers temporaires)
+/// Execute post-execution tasks, such as deleting temporary files.
 pub fn run_post_execution(file_path: &str) {
     if file_path.ends_with("_utf8.csv") {
         std::fs::remove_file(file_path).unwrap_or_else(|e| {
             log_and_print_message(
-                format!(
-                    "Une erreur est survenue lors de la suppression du fichier temporaire: {e}"
-                )
-                .as_str(),
+                format!("Error while deleting the temporary file {e}",).as_str(),
                 LogLevel::Error,
             )
         });
     }
 }
 
-/// Depuis un chemin de fichier, retourne le nom du fichier sans son extension.
+/// Extract the file name without the extension from a given file path.
 pub fn get_file_name(file_path: &str) -> &str {
     file_path
         .rsplit(&['\\', '/'] as &[char])
@@ -169,12 +166,12 @@ pub fn get_file_name(file_path: &str) -> &str {
         .unwrap()
 }
 
-/// Vérifie si un fichier existe à un chemin donné.
+/// Check if a file exists at the given path and log an error message if it does not.
 pub fn file_exists(file_path: &str) -> bool {
     let exist: bool = std::path::Path::new(file_path).exists();
     if !exist {
         log_and_print_message(
-            format!("Le fichier {file_path} n'existe pas").as_str(),
+            format!("File {file_path} does not exist").as_str(),
             LogLevel::Error,
         );
     }
